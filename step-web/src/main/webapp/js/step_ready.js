25f170cceca45d7dd602731e0ec11eadbbafa9a4
Evan Tang <patricksptang@gmail.com>
2022-10-11 00:30:31 -0700

    temp change

--- step-web/src/main/webapp/js/step_ready.js
+++ step-web/src/main/webapp/js/step_ready.js
@@ -143,6 +143,13 @@
                             }
                         }
 					}
+                    else if (code == 67) {
+                        var tmp = $(".passageContainer").clone();
+                        $(tmp).find(".notesPane").remove();
+                        $(tmp).find(".note").remove();
+                        console.log ($(tmp).text());
+                        window.navigator.clipboard.writeText($(tmp).text());
+                    }
 				}
 			});
 		}
